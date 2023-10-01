21fb59e03933da69d4f936eda82b57b375f9f886
Humdaan Mirza <mirzahumdaan786@gmail.com>
2023-10-01 19:45:21 +0530

    Added Linking to and cutomizing for smallerdevices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {FaSearch} from 'react-icons/fa'
 export default function Header() {
   return (
@@ -9,12 +10,34 @@ export default function Header() {
         <span className='text-slate-500'>Raipur</span>
         <span className='text-slate-700'>Realtor</span>
       </h1>
-      <form className='bg-slate-100 p-3 flex items-center   rounded-lg '>
+      <form className='bg-slate-100 p-3 flex items-center  rounded-lg '>
       <input type='text' placeholder='Search...'
       //  sm: code.. if for small scxreen devices  
-      className='bg-transparent focus:outline-none w-50 sm: w-60'/>
+      className='bg-transparent focus:outline-none w-50 sm: w-40'/>
       <FaSearch className='text-slate-600 '/>
       </form>
+      <ul className='flex gap-4'>
+        <Link to='/home'>
+        <li className='hidden sm:inline text-slate-700 hover:underline'>
+        Home
+        </li>
+        </Link>
+        <Link to='/about'>
+        <li className='hidden sm:inline text-slate-700 hover:underline'>
+        About
+        </li>
+        </Link>
+        <Link to='/sign-In'>
+        <li className='sm:inline text-slate-700 hover:underline'>
+        SignIn
+        </li>
+        </Link>
+        {/* <Link to='/home'>
+        <li className='hidden sm:inline text-slate-700 hover:underline'>
+        SignOut
+        </li>
+        </Link> */}
+      </ul>
     </div>
    </header>
   )
