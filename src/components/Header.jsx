c2d3fc43ebedb547fb4f079ec870b78b203ac7e6
Humdaan Mirza <mirzahumdaan786@gmail.com>
2023-10-01 19:31:30 +0530

    npm insatall react-icons import FaSearch from reactIcons added search icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import {FaSearch} from 'react-icons/fa'
 export default function Header() {
   return (
    <header className='bg-slate-200 shadow-md'>
-    <div className='flex justify-between items-center max-w-6l mx-auto p-3 ' >
+    {/* <div className='flex justify-between items-center max-w-6l mx-auto p-3 ' > */}
+    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <span className='text-slate-500'>Raipur</span>
         <span className='text-slate-700'>Realtor</span>
       </h1>
-      <form>
+      <form className='bg-slate-100 p-3 flex items-center   rounded-lg '>
       <input type='text' placeholder='Search...'
-      className='bg-transparent'/>
+      //  sm: code.. if for small scxreen devices  
+      className='bg-transparent focus:outline-none w-50 sm: w-60'/>
+      <FaSearch className='text-slate-600 '/>
       </form>
     </div>
    </header>
