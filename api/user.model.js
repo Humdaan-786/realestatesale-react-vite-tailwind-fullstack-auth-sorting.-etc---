b127b4f6b7929c42b970e62cb9aef9016e5d9dbb
Humdaan Mirza <mirzahumdaan786@gmail.com>
2023-10-02 10:34:24 +0530

    Added user model

---
+++ api/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+// creating rules
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,   
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,   
+    },
+    password:{
+        type:String,
+        required:true,
+         
+    }
+    },{timestamp:true});
+//mogdb adds auto s i.e users
+const User = mongoose.model('User',userSchema);
+export default User;
