9c0a45a64908375ad3f7772de12a54f744bb2665
Humdaan Mirza <mirzahumdaan786@gmail.com>
2023-10-05 20:33:05 +0530

    Profile page private bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,10 @@ export default function App() {
       <Route path="/sign-in" element={<Signin/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/about" element={<About/>}/>
-      <Route path="/profile" element={<PrivateRoute/>}/>
+      <Route  element={<PrivateRoute/>}>
+      <Route path="/profile" element={<Profile/>}/>
+      
+      </Route>
 
       
       
